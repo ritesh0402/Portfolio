40921f989b9c46dd83cdb690aa3d9fef019cd57f
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2021-06-20 02:23:04 +0530

    17

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-burger = document.querySelector('.burger')
-hamnav = document.querySelector('.hamnav')
+alert("Hello! I am an alert box!!");
+
+burger = document.querySelector(".burger")
+hamnav = document.querySelector(".hamnav")
 
 
-burger.addEventListener('click', ()=>{
+burger.addEventListener("click", ()=>{
     hamnav.classList.toggle('visibilty');
 })
